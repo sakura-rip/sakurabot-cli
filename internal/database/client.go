afb6ec9e878e12053440b49dc670e424e3f2453e
sakura-rip <root@sakura.rip>
2021-09-01 18:24:25 +0900

    add: gorm database client

---
+++ internal/database/client.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"github.com/sakura-rip/sakurabot-cli/internal/utils"
+	"github.com/spf13/cobra"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"os"
+)
+
+func getHomeDir() string {
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	return home
+}
+
+var Client = NewDatabase(getHomeDir()+"/.sakurabot-manager.db", false)
+
+func NewDatabase(path string, useMySql bool) *gorm.DB {
+	var db *gorm.DB
+	var err error
+	if useMySql {
+		db, err = gorm.Open(mysql.Open(os.ExpandEnv("${MYSQL_USER_NAME}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DBNAME}?charset=utf8mb4&parseTime=True&loc=Local")), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Silent),
+		})
+	} else {
+		db, err = gorm.Open(sqlite.Open(path), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Silent),
+		})
+	}
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&Tag{}, &User{}, &String{})
+	if err != nil {
+		utils.Logger.Error().Err(err).Msg("")
+	}
+	return db
+}
