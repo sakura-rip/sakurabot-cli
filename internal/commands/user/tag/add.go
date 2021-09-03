ab5011506d5e7e0e406b02c9e24889490f5e3d78
sakura-rip <root@sakura.rip>
2021-09-03 14:58:18 +0900

    add: user tag add command

---
+++ internal/commands/user/tag/add.go
@@ -0,0 +1,107 @@
+package tag
+
+import (
+	"github.com/sakura-rip/sakurabot-cli/internal/actor"
+	"github.com/sakura-rip/sakurabot-cli/internal/database"
+	"github.com/sakura-rip/sakurabot-cli/internal/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"gopkg.in/go-playground/validator.v9"
+	"strconv"
+	"strings"
+)
+
+var addParam = new(addParams)
+
+// AddCommand base command for "user tag add"
+func AddCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add",
+		Short: "add user tag",
+		Run:   runAddCommand,
+	}
+	cmd.Flags().AddFlagSet(addParam.getFlagSet())
+	return cmd
+}
+
+// addParams add commands parameter
+type addParams struct {
+	userId int
+	tags   []string
+}
+
+// getFlagSet returns the flagSet for addParams
+func (p *addParams) getFlagSet() *pflag.FlagSet {
+	fs := new(pflag.FlagSet)
+	fs.IntVarP(&addParam.userId, "userid", "u", 0, "user id")
+	fs.StringArrayVarP(&addParam.tags, "tags", "t", []string{}, "tags")
+	return fs
+}
+
+// validate validate parameters
+func (p *addParams) validate() error {
+	return validator.New().Struct(p)
+}
+
+// processParams process parameters variable
+func (p *addParams) processParams(args []string) {
+	if err := p.validate(); err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+}
+
+// processInteract process interact parameter initializer
+func (p *addParams) processInteract(args []string) {
+	userId, err := actor.Actor.PromptAndRetry(actor.Input("user id"), actor.CheckIsAPositiveNumber, func(s string) error {
+		user, err := database.GetUser(s)
+		if err != nil {
+			return err
+		}
+		utils.Logger.Info().Msgf("user name: %v", user.Name)
+		return nil
+	})
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	n, _ := strconv.Atoi(userId)
+	p.userId = n
+
+	tags, err := actor.Actor.Prompt(actor.Input("user tags"), actor.CheckNotEmpty)
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	if tags != "" {
+		p.tags = strings.Split(tags, ",")
+	}
+}
+
+func (p *addParams) DBTags() []*database.Tag {
+	var tags []*database.Tag
+	for _, tagName := range p.tags {
+		var tag *database.Tag
+		result := database.Client.Where(&database.Tag{Name: tagName}).First(tag)
+		if result.RowsAffected == 0 {
+			tag = &database.Tag{Name: tagName}
+			database.Client.Create(tag)
+		}
+		tags = append(tags, tag)
+	}
+	return tags
+}
+
+// runAddCommand execute "user tag add" command
+func runAddCommand(cmd *cobra.Command, args []string) {
+	if cmd.Flags().NFlag() == 0 {
+		addParam.processInteract(args)
+	}
+	addParam.processParams(args)
+	user, err := database.GetUser(addParam.userId)
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	err = database.Client.Model(user).Association("Tags").Append(addParam.DBTags())
+	if err != nil {
+		utils.Logger.Error().Err(err).Msg("")
+	}
+	utils.Logger.Info().Msgf("DONE: add %v tags to user: [%v]", len(addParam.tags), user.Name)
+}
