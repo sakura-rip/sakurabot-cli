701c7dd58f1bdae3541c043ea0d3ce13c27976bd
sakura-rip <root@sakura.rip>
2021-09-03 14:58:05 +0900

    add: user tag command

---
+++ internal/commands/user/tag/root.go
@@ -0,0 +1,15 @@
+package tag
+
+import "github.com/spf13/cobra"
+
+// TagCommand creates the "user tag" command
+func TagCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tag",
+		Short: "tag management",
+	}
+	cmd.AddCommand(
+		AddCommand(),
+	)
+	return cmd
+}
