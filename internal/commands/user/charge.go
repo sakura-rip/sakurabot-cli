87de832d87cbc3601527b4eb9a49adfbac779005
sakura-rip <root@sakura.rip>
2021-09-03 14:43:00 +0900

    add: charge command

---
+++ internal/commands/user/charge.go
@@ -0,0 +1,117 @@
+package user
+
+import (
+	"errors"
+	"github.com/sakura-rip/sakurabot-cli/internal/actor"
+	"github.com/sakura-rip/sakurabot-cli/internal/database"
+	"github.com/sakura-rip/sakurabot-cli/internal/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"gopkg.in/go-playground/validator.v9"
+	"strconv"
+)
+
+var chargeParam = new(chargeParams)
+
+// ChargeCommand base command for "user charge"
+func ChargeCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "charge",
+		Short: "charge user",
+		Run:   runChargeCommand,
+	}
+	cmd.Flags().AddFlagSet(chargeParam.getFlagSet())
+	return cmd
+}
+
+// chargeParams add commands parameter
+type chargeParams struct {
+	userId     int
+	amount     int
+	chargeType string
+}
+
+// getFlagSet returns the flagSet for chargeParams
+func (p *chargeParams) getFlagSet() *pflag.FlagSet {
+	fs := new(pflag.FlagSet)
+	fs.IntVarP(&chargeParam.userId, "userid", "u", 0, "user id to charge")
+	fs.IntVarP(&chargeParam.amount, "amount", "a", 0, "amount to charge")
+	fs.StringVarP(&chargeParam.chargeType, "type", "t", "", "charge type")
+	return fs
+}
+
+// validate validate parameters
+func (p *chargeParams) validate() error {
+	return validator.New().Struct(p)
+}
+
+// processParams process parameters variable
+func (p *chargeParams) processParams(args []string) {
+	if err := p.validate(); err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+}
+
+// processInteract process interact parameter initializer
+func (p *chargeParams) processInteract(args []string) {
+	uid, err := actor.Actor.PromptAndRetry(actor.Input("user id "), actor.CheckNotEmpty, func(s string) error {
+		user, err := database.GetUser(s)
+		if err != nil {
+			return err
+		}
+		utils.Logger.Info().Msgf("user name: %v", user.Name)
+		return nil
+	})
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	u, _ := strconv.Atoi(uid)
+	p.userId = u
+
+	amount, err := actor.Actor.PromptAndRetry(actor.Input("amount"), func(s string) error {
+		if n, err := strconv.Atoi(s); err != nil {
+			return err
+		} else if n <= 0 {
+			return errors.New("the amount must be at least 1")
+		}
+		return nil
+	})
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	n, _ := strconv.Atoi(amount)
+	p.amount = n
+
+	type_, err := actor.Actor.PromptOptional(actor.Input("type"), "amazon")
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	p.chargeType = type_
+
+}
+
+// runChargeCommand execute "user charge" command
+func runChargeCommand(cmd *cobra.Command, args []string) {
+	if cmd.Flags().NFlag() == 0 {
+		chargeParam.processInteract(args)
+	}
+	chargeParam.processParams(args)
+
+	charge := &database.Charge{
+		Amount: chargeParam.amount,
+		Type:   database.ChargeType(chargeParam.chargeType),
+		UserId: chargeParam.userId,
+	}
+
+	user, err := database.GetUser(chargeParam.userId)
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("")
+	}
+	user.Balance += chargeParam.amount
+	database.Client.Save(user)
+
+	err = database.Client.Model(user).Association("Charges").Append(charge)
+	if err != nil {
+		utils.Logger.Error().Err(err).Msg("")
+	}
+}
