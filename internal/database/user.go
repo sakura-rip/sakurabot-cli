69f8531c2bb5057c9b1953234d0d200538dc7ca3
sakura-rip <root@sakura.rip>
2021-09-04 16:09:52 +0900

    change: move to utils file

--- internal/database/user.go
+++ internal/database/user.go
@@ -1,15 +1,10 @@
 package database
 
 import (
-	"errors"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
-	"os"
-	"strings"
-
-	"github.com/jedib0t/go-pretty/v6/table"
 )
 
+// String database table to save string array object
 type String struct {
 	*gorm.Model
 
@@ -17,6 +12,7 @@ type String struct {
 	Value   string
 }
 
+// User database table for user
 type User struct {
 	*gorm.Model
 
@@ -27,57 +23,4 @@ type User struct {
 	Email   string
 	Balance int
 	Group   string
-}
-
-// GetTags return the string array of user tag
-func (u *User) GetTags() []string {
-	var tags []string
-	for _, t := range u.Tags {
-		tags = append(tags, t.Name)
-	}
-	return tags
-}
-
-// GetMids return the string array of user mid
-func (u *User) GetMids() []string {
-	var mids []string
-	for _, m := range u.Mids {
-		mids = append(mids, m.Value)
-	}
-	return mids
-}
-
-// Print output user information to console
-func (u *User) Print() {
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"id", "name", "tags", "email", "balance", "group"})
-	t.AppendRows([]table.Row{{u.ID, u.Name, strings.Join(u.GetTags(), ","), u.Email, u.Balance, u.Group}})
-	t.AppendSeparator()
-	t.Render()
-}
-
-// PrintUsers output users information given to args to console
-func PrintUsers(users []*User) {
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"id", "name", "tags", "email", "balance", "group"})
-	var data []table.Row
-	for _, u := range users {
-		data = append(data, table.Row{
-			u.ID, u.Name, strings.Join(u.GetTags(), ","), u.Email, u.Balance, u.Group,
-		})
-	}
-	t.AppendRows(data)
-	t.AppendSeparator()
-	t.Render()
-}
-
-func GetUser(id interface{}) (*User, error) {
-	var user *User
-	result := Client.Preload(clause.Associations).First(&user, id)
-	if result.RowsAffected == 0 {
-		return nil, errors.New("no user found")
-	}
-	return user, nil
 }
