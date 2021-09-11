401f4924164d9ef8d8f2887c75ac8ace65bc8052
sakura-rip <root@sakura.rip>
2021-09-12 01:41:59 +0900

    add: print product funcs

---
+++ internal/database/product_util.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"github.com/jedib0t/go-pretty/v6/table"
+	"os"
+	"strings"
+)
+
+// GetTags return the string array of user tag
+func (p *Product) GetTags() []string {
+	var tags []string
+	for _, t := range p.Tags {
+		tags = append(tags, t.Name)
+	}
+	return tags
+}
+
+func (p *Product) Print() {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"id", "name", "description", "tags", "price"})
+	t.AppendRows([]table.Row{{p.ID, p.Name, p.Description, strings.Join(p.GetTags(), ","), p.Price}})
+	t.AppendSeparator()
+	t.Render()
+}
+
+// PrintProducts output users information given to args to console
+func PrintProducts(users []*Product) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"id", "name", "tags", "email", "balance", "group"})
+	var data []table.Row
+	for _, p := range users {
+		data = append(data, table.Row{p.ID, p.Name, p.Description, strings.Join(p.GetTags(), ","), p.Price})
+	}
+	t.AppendRows(data)
+	t.AppendSeparator()
+	t.Render()
+}
